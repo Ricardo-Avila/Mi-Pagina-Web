dd9b85f1947a21d3d67c80587581ddbf663e2521
Ricardo Avila <rickyavilagu22@gmail.com>
2026-05-21 22:12:51 -0600

    Actualizar rutas y detalles del viaje

--- soundcloud-player.js
+++ soundcloud-player.js
@@ -8,19 +8,27 @@ document.addEventListener("DOMContentLoaded", () => {
     const widget = window.SC.Widget(miniWidget);
     let isReady = false;
 
-    widget.bind(window.SC.Widget.Events.READY, () => {
-        isReady = true;
-    });
-
-    function startAfterInteraction() {
+    function tryPlay() {
         if (isReady) {
             widget.play();
         }
+    }
+
+    widget.bind(window.SC.Widget.Events.READY, () => {
+        isReady = true;
+        tryPlay();
+        setTimeout(tryPlay, 800);
+    });
+
+    function startAfterInteraction() {
+        tryPlay();
 
         document.removeEventListener("click", startAfterInteraction);
         document.removeEventListener("touchstart", startAfterInteraction);
+        document.removeEventListener("keydown", startAfterInteraction);
     }
 
     document.addEventListener("click", startAfterInteraction, { passive: true });
     document.addEventListener("touchstart", startAfterInteraction, { passive: true });
+    document.addEventListener("keydown", startAfterInteraction);
 });
